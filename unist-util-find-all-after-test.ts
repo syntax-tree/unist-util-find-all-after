761950597c6cc58fff09ebc653ecbdfb318fe1b7
Lucas <git@roang.info>
2019-12-08 19:21:48 +0100

    Add types
    Closes GH-7.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

---
+++ unist-util-find-all-after-test.ts
@@ -0,0 +1,37 @@
+import {Node, Parent} from 'unist'
+import findAllAfter = require('unist-util-find-all-after')
+
+const heading: Node = {
+  type: 'heading',
+  depth: 2,
+  children: []
+}
+
+const parent: Parent = {
+  type: 'root',
+  children: [heading]
+}
+
+/*=== missing params ===*/
+// $ExpectError
+findAllAfter()
+// $ExpectError
+findAllAfter(parent)
+
+/*=== find by index/node ===*/
+findAllAfter(parent, 1)
+findAllAfter(parent, heading)
+// $ExpectError
+findAllAfter(parent, false)
+
+/*=== find with test ===*/
+// $ExpectError
+findAllAfter(parent, 1, false)
+findAllAfter(parent, 1, 'paragraph')
+
+/*=== invalid return ===*/
+// $ExpectError
+const returnIsString: string = findAllAfter(parent, 1)
+
+/*=== valid return ===*/
+const nodes: Node[] = findAllAfter(parent, 1)
