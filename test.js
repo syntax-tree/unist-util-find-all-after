c2e051b773821637b028455e7f08d77279b85d46
Titus Wormer <tituswormer@gmail.com>
2023-01-24 17:47:12 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -3,7 +3,8 @@
  * @typedef {import('mdast').Paragraph} Paragraph
  */
 
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {fromMarkdown} from 'mdast-util-from-markdown'
 import {findAllAfter} from './index.js'
 
@@ -11,8 +12,8 @@ const tree = fromMarkdown('Some *emphasis*, **importance**, and `code`.')
 const paragraph = /** @type {Paragraph} */ (tree.children[0])
 const children = paragraph.children
 
-test('unist-util-find-all-after', (t) => {
-  t.throws(
+test('unist-util-find-all-after', () => {
+  assert.throws(
     () => {
       // @ts-expect-error runtime.
       findAllAfter()
@@ -21,7 +22,7 @@ test('unist-util-find-all-after', (t) => {
     'should fail without parent'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       // @ts-expect-error runtime.
       findAllAfter({type: 'foo'})
@@ -30,7 +31,7 @@ test('unist-util-find-all-after', (t) => {
     'should fail without parent node'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       // @ts-expect-error runtime.
       findAllAfter({type: 'foo', children: []})
@@ -39,7 +40,7 @@ test('unist-util-find-all-after', (t) => {
     'should fail without index'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       findAllAfter({type: 'foo', children: []}, -1)
     },
@@ -47,7 +48,7 @@ test('unist-util-find-all-after', (t) => {
     'should fail with invalid index (#1)'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       findAllAfter({type: 'foo', children: []}, Number.POSITIVE_INFINITY)
     },
@@ -55,7 +56,7 @@ test('unist-util-find-all-after', (t) => {
     'should fail with invalid index (#2)'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       // @ts-expect-error runtime.
       findAllAfter({type: 'foo', children: []}, false)
@@ -64,7 +65,7 @@ test('unist-util-find-all-after', (t) => {
     'should fail with invalid index (#3)'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       findAllAfter({type: 'foo', children: []}, -1)
     },
@@ -72,7 +73,7 @@ test('unist-util-find-all-after', (t) => {
     'should fail with invalid index (#4)'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       findAllAfter({type: 'foo', children: []}, {type: 'bar'})
     },
@@ -80,7 +81,7 @@ test('unist-util-find-all-after', (t) => {
     'should fail with invalid index (#5)'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       findAllAfter(
         {type: 'foo', children: [{type: 'bar'}, {type: 'baz'}]},
@@ -93,7 +94,7 @@ test('unist-util-find-all-after', (t) => {
     'should fail for invalid `test` (#1)'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       findAllAfter(
         {type: 'foo', children: [{type: 'bar'}, {type: 'baz'}]},
@@ -106,108 +107,106 @@ test('unist-util-find-all-after', (t) => {
     'should fail for invalid `test` (#2)'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     findAllAfter(paragraph, children[1]),
     children.slice(2),
     'should return the following node when without `test` (#1)'
   )
-  t.deepEqual(
+  assert.deepEqual(
     findAllAfter(paragraph, 1),
     children.slice(2),
     'should return the following node when without `test` (#1)'
   )
-  t.deepEqual(
+  assert.deepEqual(
     findAllAfter(paragraph, 7),
     [],
     'should return the following node when without `test` (#1)'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     // @ts-expect-error TS doesn’t understand nodes.
     findAllAfter(paragraph, 0, children[6]),
     [children[6]],
     'should return `node` when given a `node` and existing (#1)'
   )
-  t.deepEqual(
+  assert.deepEqual(
     // @ts-expect-error TS doesn’t understand nodes.
     findAllAfter(paragraph, children[0], children[1]),
     [children[1]],
     'should return `node` when given a `node` and existing (#2)'
   )
-  t.deepEqual(
+  assert.deepEqual(
     // @ts-expect-error TS doesn’t understand nodes.
     findAllAfter(paragraph, 0, children[1]),
     [children[1]],
     'should return `node` when given a `node` and existing (#3)'
   )
-  t.deepEqual(
+  assert.deepEqual(
     // @ts-expect-error TS doesn’t understand nodes.
     findAllAfter(paragraph, children[0], children[0]),
     [],
     'should return `node` when given a `node` and existing (#4)'
   )
-  t.deepEqual(
+  assert.deepEqual(
     // @ts-expect-error TS doesn’t understand nodes.
     findAllAfter(paragraph, 0, children[0]),
     [],
     'should return `node` when given a `node` and existing (#5)'
   )
-  t.deepEqual(
+  assert.deepEqual(
     // @ts-expect-error TS doesn’t understand nodes.
     findAllAfter(paragraph, 1, children[1]),
     [],
     'should return `node` when given a `node` and existing (#6)'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     findAllAfter(paragraph, 0, 'strong'),
     [children[3]],
     'should return a child when given a `type` and existing (#1)'
   )
-  t.deepEqual(
+  assert.deepEqual(
     findAllAfter(paragraph, 3, 'strong'),
     [],
     'should return a child when given a `type` and existing (#2)'
   )
-  t.deepEqual(
+  assert.deepEqual(
     findAllAfter(paragraph, children[0], 'strong'),
     [children[3]],
     'should return a child when given a `type` and existing (#3)'
   )
-  t.deepEqual(
+  assert.deepEqual(
     findAllAfter(paragraph, children[3], 'strong'),
     [],
     'should return a child when given a `type` and existing (#4)'
   )
 
-  t.deepEqual(
-    findAllAfter(paragraph, 0, test),
+  assert.deepEqual(
+    findAllAfter(paragraph, 0, check),
     children.slice(5),
     'should return a child when given a `test` and existing (#1)'
   )
-  t.deepEqual(
-    findAllAfter(paragraph, 6, test),
+  assert.deepEqual(
+    findAllAfter(paragraph, 6, check),
     [],
     'should return a child when given a `test` and existing (#2)'
   )
-  t.deepEqual(
-    findAllAfter(paragraph, children[4], test),
+  assert.deepEqual(
+    findAllAfter(paragraph, children[4], check),
     children.slice(5),
     'should return a child when given a `test` and existing (#3)'
   )
-  t.deepEqual(
-    findAllAfter(paragraph, children[6], test),
+  assert.deepEqual(
+    findAllAfter(paragraph, children[6], check),
     [],
     'should return a child when given a `test` and existing (#4)'
   )
 
   /**
    * @param {Node} _
-   * @param {number|null|undefined} n
+   * @param {number | null | undefined} n
    */
-  function test(_, n) {
+  function check(_, n) {
     return typeof n === 'number' && n >= 5
   }
-
-  t.end()
 })
